c0cafb99578818ef8d906e58524a0118de73d648
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-02-26 18:21:36 +0200

    Fix: added missing imports, and enhanced the overall structure.
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -32,21 +32,20 @@ import {
 } from "@solana/spl-token-metadata";
 
 // Constants and Configuration
-const FEE_BASIS_POINTS = 300;
-const MAX_FEE = BigInt(800);
-const MINT_AMOUNT = 20_000_000_000_000n;
-const TRANSFER_AMOUNT = BigInt(100000);
-const DECIMALS = 3;
-const COMMITMENT_LEVEL = "confirmed";
-const clusterUrl = clusterApiUrl("devnet");
-const TOKEN_2022_PROGRAM_ID = new PublicKey("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb");
-
-// Define LAMPORTS_PER_SOL
-const LAMPORTS_PER_SOL = 1000000000; // 1 SOL = 1,000,000,000 lamports
-
-// Burning Mechanism and Configuration
-const BURN_START_QUARTER = 3; // Burning starts on Q3, 2024
-const BURN_RATE = 0.02; // 2% quarterly burning rate
+const config = {
+  FEE_BASIS_POINTS: 600,
+  MAX_FEE: BigInt(800),
+  MINT_AMOUNT: 20_000_000_000_000n,
+  TRANSFER_AMOUNT: BigInt(100_000),
+  DECIMALS: 3,
+  COMMITMENT_LEVEL: "confirmed",
+  clusterUrl: clusterApiUrl("devnet"),
+  TOKEN_2022_PROGRAM_ID: new PublicKey("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"),
+  BURN_WALLET_ADDRESS: "BURNF5qPfU1A9wSYCB4x4VUwQd398VHqwMHCCsDhp134",
+  LAMPORTS_PER_SOL: 1000000000,
+  BURN_START_QUARTER: 3,
+  BURN_RATE: 0.02,
+};
 
 // Connection to devnet cluster
 const connection = await initializeConnection();
@@ -73,7 +72,7 @@ const metaData: TokenMetadata = {
   updateAuthority: mintAuthority, // Replace with the appropriate authority
   name: "BARK",
   symbol: "BARK",
-  uri: "https://raw.githubusercontent.com/bark-community/bark-token/bob/main/bark/assets/bark.png",
+  uri: "https://raw.githubusercontent.com/bark-community/bark-token/bob/main/src/assets/bark.png",
   media: {
     website: "https://barkprotocol.net",
     socialMedia: {
@@ -81,7 +80,7 @@ const metaData: TokenMetadata = {
       discord: "https://discord.gg/bark-protocol-en",
       telegram: "https://telegram.com/t.me/bark_protocol",
       additionalMetadata: [
-        ["description", "BARK, a digital asset on the Solana blockchain, SPL token extension, and is driven by community contributions."],
+        ["description", "BARK, a digital asset on the Solana blockchain, and is driven by community contributions."],
       ],
     },
   },
@@ -90,7 +89,7 @@ const metaData: TokenMetadata = {
 // Function to initialize the Solana connection
 async function initializeConnection() {
   try {
-    return new Connection(clusterUrl, COMMITMENT_LEVEL);
+    return new Connection(config.clusterUrl, config.COMMITMENT_LEVEL);
   } catch (error) {
     console.error("Error initializing Solana connection:", error.message);
     throw new Error("Failed to initialize Solana connection");
@@ -103,15 +102,14 @@ async function createFeeAccount(payer) {
     const newFeeAccountKeypair = Keypair.generate();
     const newFeeAccount = newFeeAccountKeypair.publicKey;
 
-    const feeAccountSpace = 165; // Replace this with the actual value
-    const feeAccountLamports = await connection.getMinimumBalanceForRentExemption(feeAccountSpace);
+    const feeAccountSpace = await connection.getMinimumBalanceForRentExemption(mintLen);
 
     const createFeeAccountInstruction = SystemProgram.createAccount({
       fromPubkey: payer.publicKey,
       newAccountPubkey: newFeeAccount,
-      space: feeAccountSpace,
-      lamports: feeAccountLamports,
-      programId: TOKEN_2022_PROGRAM_ID,
+      space: mintLen,
+      lamports: lamports,
+      programId: config.TOKEN_2022_PROGRAM_ID,
     });
 
     const createFeeAccountTransaction = new Transaction().add(createFeeAccountInstruction);
@@ -153,22 +151,22 @@ async function initializeMintAccount() {
           newAccountPubkey: mint,
           space: mintLen,
           lamports,
-          programId: TOKEN_2022_PROGRAM_ID,
+          programId: config.TOKEN_2022_PROGRAM_ID,
         }),
         createInitializeTransferFeeConfigInstruction(
           mint,
           transferFeeConfigAuthority.publicKey,
           withdrawWithheldAuthority.publicKey,
-          FEE_BASIS_POINTS,
-          MAX_FEE,
-          TOKEN_2022_PROGRAM_ID,
+          config.FEE_BASIS_POINTS,
+          config.MAX_FEE,
+          config.TOKEN_2022_PROGRAM_ID,
         ),
         createInitializeMintInstruction(
           mint,
-          DECIMALS,
+          config.DECIMALS,
           mintAuthority,
           null,
-          TOKEN_2022_PROGRAM_ID,
+          config.TOKEN_2022_PROGRAM_ID,
         ),
       );
 
@@ -191,7 +189,7 @@ async function initializeSolanaAccounts() {
       sourceOwnerKeypair.publicKey,
       undefined,
       undefined,
-      TOKEN_2022_PROGRAM_ID,
+      config.TOKEN_2022_PROGRAM_ID,
     );
 
     const destinationOwnerKeypair = Keypair.generate();
@@ -202,7 +200,7 @@ async function initializeSolanaAccounts() {
       destinationOwnerKeypair.publicKey,
       undefined,
       undefined,
-      TOKEN_2022_PROGRAM_ID,
+      config.TOKEN_2022_PROGRAM_ID,
     );
 
     return [sourceTokenAccount, destinationTokenAccount];
@@ -224,7 +222,7 @@ async function transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, 
     const sourceAccountBalance = sourceAccountInfo ? sourceAccountInfo.lamports : 0;
     console.log(`Source account balance before transfer: ${sourceAccountBalance}`);
 
-    if (sourceAccountBalance < TRANSFER_AMOUNT) {
+    if (sourceAccountBalance < config.TRANSFER_AMOUNT) {
       console.error("Insufficient balance in source account for transfer.");
       return;
     }
@@ -241,12 +239,12 @@ async function transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, 
       mintAmount,
       undefined,
       undefined,
-      TOKEN_2022_PROGRAM_ID,
+      config.TOKEN_2022_PROGRAM_ID,
     );
     logTransactionDetails("Mint BARK", mintToSignature);
 
-    const fee = (TRANSFER_AMOUNT * BigInt(FEE_BASIS_POINTS)) / BigInt(10_000);
-    const feeCharged = fee > MAX_FEE ? Number(MAX_FEE) : Number(fee);
+    const fee = (config.TRANSFER_AMOUNT * BigInt(config.FEE_BASIS_POINTS)) / BigInt(10_000);
+    const feeCharged = fee > config.MAX_FEE ? Number(config.MAX_FEE) : Number(fee);
 
     const transferSignature = await transferCheckedWithFee(
       connection,
@@ -255,12 +253,12 @@ async function transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, 
       mint,
       destinationTokenAccount,
       payerWallet.publicKey,
-      TRANSFER_AMOUNT,
+      config.TRANSFER_AMOUNT,
       3,
       feeCharged,
       undefined,
       undefined,
-      TOKEN_2022_PROGRAM_ID,
+      config.TOKEN_2022_PROGRAM_ID,
     );
     logTransactionDetails("Transfer BARK", transferSignature);
   } catch (error) {
@@ -272,8 +270,8 @@ async function transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, 
 // Function to handle fee withdrawal
 async function withdrawFees(destinationTokenAccount, accountsToWithdrawFrom, isMint = false) {
   try {
-    const allAccounts = await connection.getProgramAccounts(TOKEN_2022_PROGRAM_ID, {
-      commitment: COMMITMENT_LEVEL,
+    const allAccounts = await connection.getProgramAccounts(config.TOKEN_2022_PROGRAM_ID, {
+      commitment: config.COMMITMENT_LEVEL,
       filters: [
         {
           memcmp: {
@@ -286,7 +284,7 @@ async function withdrawFees(destinationTokenAccount, accountsToWithdrawFrom, isM
 
     const filteredAccounts = allAccounts
       .filter(accountInfo => {
-        const account = unpackAccount(accountInfo.pubkey, accountInfo.account, TOKEN_2022_PROGRAM_ID);
+        const account = unpackAccount(accountInfo.pubkey, accountInfo.account, config.TOKEN_2022_PROGRAM_ID);
         const transferFeeAmount = getTransferFeeAmount(account);
         return (
           transferFeeAmount !== null &&
@@ -307,7 +305,7 @@ async function withdrawFees(destinationTokenAccount, accountsToWithdrawFrom, isM
         undefined,
         filteredAccounts.map(accountInfo => accountInfo.pubkey),
         undefined,
-        TOKEN_2022_PROGRAM_ID,
+        config.TOKEN_2022_PROGRAM_ID,
       );
       logTransactionDetails(`Withdraw Fee${isMint ? ' from Mint' : ''}`, withdrawSignature);
     } else {
@@ -323,7 +321,7 @@ async function withdrawFees(destinationTokenAccount, accountsToWithdrawFrom, isM
 async function harvestWithheldTokensToMint(mintAccount, feeAccount) {
   try {
     let feeAccountKey = new PublicKey(feeAccount);
-    const feeAccountInfo = await connection.getAccountInfo(feeAccountKey, COMMITMENT_LEVEL);
+    const feeAccountInfo = await connection.getAccountInfo(feeAccountKey, config.COMMITMENT_LEVEL);
 
     if (!feeAccountInfo) {
       console.log(`Fee account ${feeAccount} not found. Creating a new fee account...`);
@@ -331,7 +329,7 @@ async function harvestWithheldTokensToMint(mintAccount, feeAccount) {
       console.log(`Using the newly created fee account: ${newFeeAccount.toBase58()}`);
       feeAccountKey = newFeeAccount;
     } else {
-      const feeAccountData = unpackAccount(feeAccountKey, feeAccountInfo.data, TOKEN_2022_PROGRAM_ID);
+      const feeAccountData = unpackAccount(feeAccountKey, feeAccountInfo.data, config.TOKEN_2022_PROGRAM_ID);
       const withheldAmount = getTransferFeeAmount(feeAccountData)?.withheldAmount || 0;
 
       if (withheldAmount > 0) {
@@ -346,7 +344,7 @@ async function harvestWithheldTokensToMint(mintAccount, feeAccount) {
           0,
           undefined,
           undefined,
-          TOKEN_2022_PROGRAM_ID,
+          config.TOKEN_2022_PROGRAM_ID,
         );
         logTransactionDetails(`Harvested ${withheldAmount} fees to Mint BARK`, transferSignature);
       } else {
@@ -364,8 +362,8 @@ async function checkBarkBalance() {
   try {
     const ownerPublicKey = payerWallet.publicKey;
     const tokenAccounts = await connection.getParsedTokenAccountsByOwner(ownerPublicKey, {
-      programId: TOKEN_2022_PROGRAM_ID,
-      commitment: COMMITMENT_LEVEL,
+      programId: config.TOKEN_2022_PROGRAM_ID,
+      commitment: config.COMMITMENT_LEVEL,
     });
 
     const barkAccounts = tokenAccounts.value.filter(account => account.account.data.parsed.info.mint.equals(mint));
@@ -388,55 +386,16 @@ async function checkBarkBalance() {
 async function checkBalance() {
   try {
     const balance = await connection.getBalance(payerWallet.publicKey);
-    console.log(`BARK account balance: ${balance / LAMPORTS_PER_SOL} SOL`);
+    console.log(`BARK account balance: ${balance / config.LAMPORTS_PER_SOL} SOL`);
   } catch (error) {
     console.error("Error checking wallet balance:", error.message);
     throw new Error("Failed to check wallet balance");
   }
 }
 
-// Main function to orchestrate the entire process.
-async function main() {
-  try {
-    await checkBalance();
-    await initializeMintAccount();
-    const [sourceTokenAccount, destinationTokenAccount] = await initializeSolanaAccounts();
-    await transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, MINT_AMOUNT);
-    await withdrawFees(destinationTokenAccount, [sourceTokenAccount]);
-    await transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, MINT_AMOUNT);
-
-    const existingFeeAccount = "feehVvkc5QiSu269NzHvf2TgyvhEwC5t4UxNZeMfzFS";
-    const existingFeeAccountInfo = await connection.getAccountInfo(new PublicKey(existingFeeAccount), COMMITMENT_LEVEL);
-
-    if (!existingFeeAccountInfo) {
-      console.log(`Fee account ${existingFeeAccount} not found. Creating a new fee account...`);
-      const newFeeAccount = await createFeeAccount(payerWallet);
-      console.log(`Using the newly created fee account: ${newFeeAccount.toBase58()}`);
-    } else {
-      console.log(`Using existing fee account: ${existingFeeAccount}`);
-    }
-
-    await harvestWithheldTokensToMint(mint, existingFeeAccount);
-    await withdrawFees(destinationTokenAccount, [], true);
-
-    // Burning Mechanism
-    const currentQuarter = getCurrentQuarter();
-    if (currentQuarter >= BURN_START_QUARTER) {
-      const burnAmount = calculateBurnAmount(MINT_AMOUNT);
-      await burnTokens(sourceTokenAccount, burnAmount);
-    }
-
-  } catch (error) {
-    console.error("Main process error:", error.message);
-  }
-}
-
-// Execute the main function
-main();
-
 // Function to calculate burn amount based on burning rate
 function calculateBurnAmount(amount) {
-  return BigInt(Math.floor(Number(amount) * BURN_RATE));
+  return BigInt(Math.floor(Number(amount) * config.BURN_RATE));
 }
 
 // Function to get the current quarter
@@ -463,11 +422,49 @@ async function burnTokens(tokenAccount, burnAmount) {
       0,
       undefined,
       undefined,
-      TOKEN_2022_PROGRAM_ID,
+      config.TOKEN_2022_PROGRAM_ID,
     );
-    logTransactionDetails("Burn Tokens", burnSignature);
+    logTransactionDetails("Burn BARK Tokens", burnSignature);
   } catch (error) {
-    console.error("Error burning tokens:", error.message);
-    throw new Error("Failed to burn tokens");
+    console.error("Error burning BARK tokens:", error.message);
+    throw new Error("Failed to burn BARK tokens");
   }
 }
+
+// Main function to orchestrate the entire process.
+async function main() {
+  try {
+    await checkBalance();
+    await initializeMintAccount();
+    const [sourceTokenAccount, destinationTokenAccount] = await initializeSolanaAccounts();
+    await transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, config.MINT_AMOUNT);
+    await withdrawFees(destinationTokenAccount, [sourceTokenAccount]);
+    await transferBarkWithFee(sourceTokenAccount, destinationTokenAccount, config.MINT_AMOUNT);
+
+    const existingFeeAccount = "FEEUmDqQN9M4yQknTRYvwQhf2suJPMwcJWVtmuoRrYPM";
+    const existingFeeAccountInfo = await connection.getAccountInfo(new PublicKey(existingFeeAccount), config.COMMITMENT_LEVEL);
+
+    if (!existingFeeAccountInfo) {
+      console.log(`Fee account ${existingFeeAccount} not found. Creating a new fee account...`);
+      const newFeeAccount = await createFeeAccount(payerWallet);
+      console.log(`Using the newly created fee account: ${newFeeAccount.toBase58()}`);
+    } else {
+      console.log(`Using existing fee account: ${existingFeeAccount}`);
+    }
+
+    await harvestWithheldTokensToMint(mint, existingFeeAccount);
+    await withdrawFees(destinationTokenAccount, [], true);
+
+    // Burning Mechanism
+    const currentQuarter = getCurrentQuarter();
+    if (currentQuarter >= config.BURN_START_QUARTER) {
+      const burnAmount = calculateBurnAmount(config.MINT_AMOUNT);
+      await burnTokens(sourceTokenAccount, burnAmount);
+    }
+  } catch (error) {
+    console.error("Main process error:", error.message);
+  }
+}
+
+// Execute the main function
+main();
